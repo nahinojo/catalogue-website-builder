6050c30f8480c1a7006d6db4c7b271a289c6994b
Noah Hinojos <noah.hinojos@gmail.com>
2024-08-09 14:56:59 -0700

    feat: specifiable image container id

---
+++ TODO.md
@@ -0,0 +1,7 @@
+# TODO
+## Fixes
+
+## Chores
+- Link demo entries to main webpage.
+- Setup reading image type from json.
+## Feats
--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,12 @@
 const path = require('path');
 const fs = require('fs');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const ejs = require('ejs');
-
-
 
 // Read all JSON files in the data directory
 const dataFiles = fs.readdirSync(path.resolve(__dirname, 'src/data')).filter(file => file.endsWith('.json'));
 
 // Create an HtmlWebpackPlugin instance for each JSON file
-const htmlPlugins = dataFiles.map(file => {
+const demoEntriesHtmlPlugins = dataFiles.map(file => {
   const data = require(`./src/data/${file}`);
   return new HtmlWebpackPlugin({
     filename: `${path.basename(file, '.json')}.html`,
@@ -26,7 +23,7 @@ module.exports = {
     filename: 'junk-ignore.js', // Ignorable output file
   },
   plugins: [
-    ...htmlPlugins
+    ...demoEntriesHtmlPlugins
   ],
   mode: 'development'
 };
